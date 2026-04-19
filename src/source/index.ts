594c44f64ae1027f49f6e764644c3b74be71538a
Vinicius <vinicius.feira@gmail.com>
2026-04-19 09:19:05 -0300

    Clean up source imports and instances in index.ts
    Removed unused source imports and instances from createSources function.

--- src/source/index.ts
+++ src/source/index.ts
@@ -1,13 +1,8 @@
 import { envGet, Fetcher } from '../utils';
 import { Fembed } from './Fembed';
 import { FSHD } from './FSHD';
-import { FourKHDHub } from './FourKHDHub';
-import { HDHub4u } from './HDHub4u';
-import { RgShows } from './RgShows';
 import { Source } from './Source';
 import { Videasy } from './Videasy';
-import { VidSrc } from './VidSrc';
-import { VixSrc } from './VixSrc';
 
 export * from './Source';
 
@@ -15,12 +10,6 @@ export const createSources = (fetcher: Fetcher): Source[] => {
   const disabledSources = envGet('DISABLED_SOURCES')?.split(',') ?? [];
 
   return [
-    // multi
-    new FourKHDHub(fetcher),
-    new HDHub4u(fetcher),
-    new VixSrc(fetcher),
-    new VidSrc(),
-    new RgShows(fetcher),
     // PT (BR)
     new Fembed(fetcher),
     new FSHD(fetcher),
